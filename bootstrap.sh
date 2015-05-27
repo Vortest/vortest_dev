5dcbf8a7151a3b359c58eda15ebc2459b7b5072f
Seth Urban <sethuster@gmail.com>
2015-05-27 10:45:11 -0600

    Updated some VM stuffs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,16 @@ sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 echo "Installing php..."
 sudo yum -y install php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml
 echo "Installing Java..."
-sudo yum -y install java-1.7.0-openjdk
+sudo yum -y install java-1.8.0-openjdk
 echo "Installing git..."
 sudo yum -y install git
+echo "Installing Python..."
+sudo yum -y install Python
 
+#There is so much more we need to do here:
+#1: Figure out chef (maybe)
+#1: Clone the project folders
+#2: Set up bash to set JAVA_HOME directory
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
