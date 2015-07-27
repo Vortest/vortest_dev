4d0910cfcb2a28968160db89476878978672c6b6
Seth <sethuster@gmail.com>
2015-07-27 17:27:40 -0600

    still having a bit of an issue trying to get rbenv to install ruby for everyone

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,11 +31,12 @@ sudo yum install -y git-core zlib zlib-devel gcc-c++ patch readline readline-dev
 echo "###########################"
 echo "Installing rbenv"
 echo "###########################"
-sudo git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-echo  'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
+cd /opt
+sudo git clone git://github.com/sstephenson/rbenv.git 
+echo  'export PATH="/opt/rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
 echo  'eval "$(rbenv init -)"' >> /home/vagrant/.bash_profile
-sudo git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/vagrant/.bash_profile
+sudo git clone git://github.com/sstephenson/ruby-build.git /opt/rbenv/plugins/ruby-build
+echo 'export PATH="/opt/rbenv/plugins/ruby-build/bin:$PATH"' >> /home/vagrant/.bash_profile
 source /home/vagrant/.bash_profile
 
 echo "###########################"
@@ -52,11 +53,11 @@ echo "###########################"
 gem install rails -v 4.2.3
 #start rails with rails s -b 0.0.0.0 instead of the loopback address
 
-echo "###########################"
-echo "Installing Heroku"
-echo "###########################"
-sudo wget -c https://toolbelt.herokuapp.com/install.sh && sh install.sh
-echo 'export PATH="/usr/local/heroku/bin:$PATH"' >> /home/vagrant/.bash_profile ##Add to profile
+#echo "###########################"
+#echo "Installing Heroku"
+#echo "###########################"
+#sudo wget -c https://toolbelt.herokuapp.com/install.sh && sh install.sh
+#echo 'export PATH="/usr/local/heroku/bin:$PATH"' >> /home/vagrant/.bash_profile ##Add to profile
 
 
 #Set up directories
